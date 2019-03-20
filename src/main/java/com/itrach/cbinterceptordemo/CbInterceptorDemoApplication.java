61944fd1b5e6cdae3b5697098b93e26dbdae8829
Illia Trach <itrach@datas-tech.com>
2019-03-20 19:32:32 +0200

    Initial commit

---
+++ src/main/java/com/itrach/cbinterceptordemo/CbInterceptorDemoApplication.java
@@ -0,0 +1,13 @@
+package com.itrach.cbinterceptordemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CbInterceptorDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CbInterceptorDemoApplication.class, args);
+	}
+
+}
