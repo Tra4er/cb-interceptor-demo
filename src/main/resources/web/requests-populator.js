6b12ced0837b453c8248daf9f00cc59972daa0f3
Illia Trach <itrach@datas-tech.com>
2019-03-30 17:40:02 +0200

    Admin page looks finished.

--- src/main/resources/web/requests-populator.js
+++ src/main/resources/web/requests-populator.js
@@ -1,19 +1,114 @@
-var sendOneRequests = false;
+var timer = 1000;
+var pOne;
+var pTwo;
+var pErr;
 
-$("#button-sendTenToDell").click(function(){
+$("#button-timer").click(function () {
+    timer = $("#input-timer").val();
+})
 
-    while(sendOneRequests) {
-        $.ajax({
-            url: 'http://localhost:8080/api?param=dell',
-            type: 'GET',
-            success: function (data) {
-                $("#result").text(data);
-                console.log("HERE")
-            },
-            error: function (e) {
-                //called when there is an error
-                //console.log(e.message);
+$("#button-request").click(function () {
+    var method = "/api?param=dell";
+    $.ajax({
+        url: 'http://localhost:8080' + method,
+        type: 'GET',
+        success: function (data) {
+            if (data.code !== undefined) {
+                addConsoleLog(22, method, data.message);
+            } else {
+                addConsoleLog(null, method, data);
             }
-        });
-    }
+        },
+        error: function (e) {
+            addConsoleLog(null, "/api?param=dell", e);
+        }
+    });
 });
+
+$("#button-populate-one").click(function () {
+    var method = "/api?param=asus";
+    pOne = setInterval(function () {
+            $.ajax({
+                url: 'http://localhost:8080' + method,
+                type: 'GET',
+                success: function (data) {
+                    if (data.code !== undefined) {
+                        addConsoleLog(22, method, data.message);
+                    } else {
+                        addConsoleLog(null, method, data);
+                    }
+                },
+                error: function (e) {
+                    addConsoleLog(null, method, e);
+                }
+            })
+        },
+        timer);
+});
+
+$("#button-populate-one-stop").click(function () {
+    clearInterval(pOne);
+});
+
+$("#button-populate-two").click(function () {
+    var method = "/api?param=hp";
+    pTwo = setInterval(function () {
+            $.ajax({
+                url: 'http://localhost:8080' + method,
+                type: 'GET',
+                success: function (data) {
+                    if (data.code !== undefined) {
+                        addConsoleLog(22, method, data.message);
+                    } else {
+                        addConsoleLog(null, method, data);
+                    }
+                },
+                error: function (e) {
+                    addConsoleLog(null, method, e);
+                }
+            })
+        },
+        timer);
+});
+
+$("#button-populate-two-stop").click(function () {
+    clearInterval(pTwo);
+})
+
+$("#button-populate-err").click(function () {
+    var method = "/api/exception";
+    pErr = setInterval(function () {
+            $.ajax({
+                url: 'http://localhost:8080' + method,
+                type: 'GET',
+                success: function (data) {
+                    if (data.code !== undefined) {
+                        addConsoleLog(22, method, data.message);
+                    } else {
+                        addConsoleLog(null, method, data);
+                    }
+                },
+                error: function (e) {
+                    addConsoleLog(null, method, e);
+                }
+            })
+        },
+        timer);
+});
+
+$("#button-populate-err-stop").click(function () {
+    clearInterval(pErr);
+});
+
+var addConsoleLog = function (code, method, message) {
+    var danger = "class='table-danger'";
+    if (!code) {
+        code = "~";
+        danger = "";
+    }
+    $("#table-console").prepend($("<tr " + danger + ">\n" +
+        "                    <th scope=\"row\">" + code + "</th>\n" +
+        "                    <td>" + method + "</td>\n" +
+        "                    <td>" + message + "</td>\n" +
+        "                </tr>"))
+}
--- src/main/resources/web/settings.js
+++ src/main/resources/web/settings.js
@@ -1,9 +1,13 @@
 var configParam = function(key, value) {
     return $("<div class=\"form-group\">\n" +
         "    <label for=\"exampleFormControlInput1\">" + key + "</label>\n" +
-        "    <input type=\"text\" class=\"form-control\" id=\"exampleFormControlInput1\" value=\"" + value + "\">\n" +
+        "    <input type=\"text\" class=\"form-control\" id=\"" + key + "\" value=\"" + value + "\" >\n" +
         "  </div>")
 }
+
+$.ajaxSetup({
+    contentType: "application/json; charset=utf-8"
+});
 
 $(document).ready(function () {
     $.ajax({
@@ -22,9 +26,31 @@ $(document).ready(function () {
 });
 
 $("#form").submit(function(e) {
+    var inputs = $("#form :input");
+    console.log(inputs)
     e.preventDefault();
     console.log("HERE")
-
+    var request = {};
+    inputs.each(function(index){
+        if (index === 0)
+            return;
+        // console.log(value.map(function(i, v){ return v}))
+        request[this.id] = this.value;
+    })
+    console.log(request)
+    $.ajax({
+        url: 'http://localhost:8080/api/configs',
+        type: 'POST',
+        data: JSON.stringify(request),
+        success: function(data) {
+            $("#result").text(data);
+            console.log("HERE")
+        },
+        error: function(e) {
+            //called when there is an error
+            //console.log(e.message);
+        }
+    });
 });
 
 $("#button-sendTenToDell").click(function(){
