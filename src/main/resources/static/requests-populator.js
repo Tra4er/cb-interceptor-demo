279c671024d104f6dc2e1c0859b13d878d3e20c5
Illia Trach <itrach@datas-tech.com>
2019-04-03 09:48:38 +0300

    Added timeout to UI.

--- src/main/resources/static/requests-populator.js
+++ src/main/resources/static/requests-populator.js
@@ -1,6 +1,8 @@
+var host = 'http://localhost:8080';
 var timer = 1000;
 var pOne;
 var pTwo;
+var pTimeout;
 var pErr;
 
 $("#button-timer").click(function () {
@@ -9,41 +11,14 @@ $("#button-timer").click(function () {
 
 $("#button-request").click(function () {
     var method = "/api?param=dell";
-    $.ajax({
-        url: 'http://localhost:8080' + method,
-        type: 'GET',
-        success: function (data) {
-            if (data.code !== undefined) {
-                addConsoleLog(22, method, data.message);
-            } else {
-                addConsoleLog(null, method, data);
-            }
-        },
-        error: function (e) {
-            addConsoleLog(null, "/api?param=dell", e);
-        }
-    });
+    makeRequest(method);
 });
 
 $("#button-populate-one").click(function () {
     var method = "/api?param=asus";
     pOne = setInterval(function () {
-            $.ajax({
-                url: 'http://localhost:8080' + method,
-                type: 'GET',
-                success: function (data) {
-                    if (data.code !== undefined) {
-                        addConsoleLog(22, method, data.message);
-                    } else {
-                        addConsoleLog(null, method, data);
-                    }
-                },
-                error: function (e) {
-                    addConsoleLog(null, method, e);
-                }
-            })
-        },
-        timer);
+        makeRequest(method)
+    }, timer);
 });
 
 $("#button-populate-one-stop").click(function () {
@@ -53,52 +28,52 @@ $("#button-populate-one-stop").click(function () {
 $("#button-populate-two").click(function () {
     var method = "/api?param=hp";
     pTwo = setInterval(function () {
-            $.ajax({
-                url: 'http://localhost:8080' + method,
-                type: 'GET',
-                success: function (data) {
-                    if (data.code !== undefined) {
-                        addConsoleLog(22, method, data.message);
-                    } else {
-                        addConsoleLog(null, method, data);
-                    }
-                },
-                error: function (e) {
-                    addConsoleLog(null, method, e);
-                }
-            })
-        },
-        timer);
+        makeRequest(method)
+    }, timer);
 });
 
 $("#button-populate-two-stop").click(function () {
     clearInterval(pTwo);
 })
 
+$("#button-populate-timeout").click(function () {
+    var method = "/api/timeout";
+    pTimeout = setInterval(function () {
+        makeRequest(method)
+    }, timer);
+});
+
+$("#button-populate-timeout-stop").click(function () {
+    clearInterval(pTimeout);
+})
+
 $("#button-populate-err").click(function () {
     var method = "/api/exception";
     pErr = setInterval(function () {
-            $.ajax({
-                url: 'http://localhost:8080' + method,
-                type: 'GET',
-                success: function (data) {
-                    if (data.code !== undefined) {
-                        addConsoleLog(22, method, data.message);
-                    } else {
-                        addConsoleLog(null, method, data);
-                    }
-                },
-                error: function (e) {
-                    addConsoleLog(null, method, e);
-                }
-            })
-        },
-        timer);
+        makeRequest(method)
+    }, timer);
 });
 
 $("#button-populate-err-stop").click(function () {
     clearInterval(pErr);
 });
+
+var makeRequest = function (method) {
+    $.ajax({
+        url: host + method,
+        type: 'GET',
+        success: function (data) {
+            if (data.code !== undefined) {
+                addConsoleLog(data.code, method, data.message);
+            } else {
+                addConsoleLog(null, method, data);
+            }
+        },
+        error: function (e) {
+            addConsoleLog(null, method, e);
+        }
+    })
+}
 
 var addConsoleLog = function (code, method, message) {
     var danger = "class='table-danger'";
